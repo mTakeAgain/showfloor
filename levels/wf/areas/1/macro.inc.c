9845692b38c9ae551b87410952e5299232f3a6ff
The1UPGuy <75804989+The1UPGuy@users.noreply.github.com>
2025-10-30 21:59:33 -0300

    wf updates + tweaked ddd map settings
    + removed camera buzz

--- levels/wf/areas/1/macro.inc.c
+++ levels/wf/areas/1/macro.inc.c
@@ -1,13 +1,16 @@
 const MacroObject mountain_macro[] = {
     MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 1720, 2560,  2250), //top right coin
-    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 1740, 2560,  2505), //top left coin
-    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 1920, 2560,  2540), //bottom left coin
-    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1184, 1024,  3900),
-    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1395, 1024,  3900),
-    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1606, 1024,  3900),
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 1740, 2560,  2536), //top left coin
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 1924, 2560,  2558), //bottom left coin
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1164, 1024,  3890), // cannon coin right
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1365, 1024,  3890), // cannon coin middle
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-1576, 1024,  3890), // cannon coin left
     MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 3840, 1075,  -1791),
     MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 3840, 1075,  -767),
     MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/ 3840, 1075,  -2815),
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-2850, 2561,   190),
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-2855, 2561,  -260),
+    MACRO_OBJECT(/*preset*/ macro_yellow_coin_1,        /*yaw*/   0, /*pos*/-2860, 2561,  -710),
     MACRO_OBJECT_WITH_BHV_PARAM(/*preset*/ macro_cannon_open,               /*yaw*/   0, /*pos*/ -1844,  1026,  3893, /*bhvParam*/ 0x40),
     MACRO_OBJECT_END(),
 };
